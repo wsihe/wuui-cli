0a3c25808992bdf5b69b8c00e9833cb6d98c1a6a
river <837606100@qq.com>
2017-11-27 11:19:40 +0800

    fix:修改module模块

--- commands/module.js
+++ commands/module.js
@@ -20,7 +20,6 @@ var MModule = Base.extend({
    * @constructor
    * @param {Object} options
    * @param {String} [options.moduleName] - 模块名称
-   * @param {String} [options.moduleDescription] - 模块描述
    */
   construct: function (options) {
     this.conf = _.assign({
@@ -32,21 +31,13 @@ var MModule = Base.extend({
   },
 
   init: function () {
-    this.gConfig = Util.getConfig();
-    var userHome = Util.homedir();
-    if (this.gConfig.user_name) {
-      this.userName = this.gConfig.user_name;
-    } else {
-      this.needSetUsername = true;
-      this.userName = process.env.USER || path.basename(userHome);
-    }
-    this.appConfPath = this.destinationPath('app-conf.js');
+    this.userName = process.env.USER || path.basename(userHome);
     console.log(chalk.magenta(this.userName + '开始创建模块!'));
   },
 
   /**
    * @desc 输出询问信息
-   * @param {Function} cb - 输入完后的回调
+   * @param {Function} cb - 回调
    */
   talk: function (cb) {
     var prompts = [];
@@ -55,13 +46,13 @@ var MModule = Base.extend({
       prompts.push({
         type: 'input',
         name: 'moduleName',
-        message: '+++~',
+        message: '请输入模块的名字！',
         validate: function(input) {
           if (!input) {
-            return '不能为空哦，会让人家很为难的~';
+            return '模块的名字不能为空！';
           }
           if (fs.existsSync(this.destinationPath(input))) {
-            return '模块已经存在哦，如果你只想增加页面，请使用 wuui -p 页面名~';
+            return '模块已经存在，如果你只想增加页面，请使用 wuui -p 页面名~';
           }
           return true;
         }.bind(this)
@@ -70,7 +61,7 @@ var MModule = Base.extend({
       prompts.push({
         type: 'input',
         name: 'moduleName',
-        message: '模块已经存在哦，换个名字吧~',
+        message: '模块已经存在，会在该模块下直接创建页面~',
         validate: function(input) {
           if (!input) {
             return '不能为空哦，会让人家很为难的~';
@@ -83,57 +74,34 @@ var MModule = Base.extend({
       });
     }
 
-    inquirer.prompt(prompts, function(answers) {
+    inquirer.prompt(prompts).then((answers) => {
       if (!answers.author) {
         answers.author = this.userName;
       }
       _.assign(this.conf, answers);
       this.write(cb);
-    }.bind(this));
+    })
   },
 
   /**
-   * @desc 创建目录，拷贝模板
-   * @param {Function} cb - 创建完后的回调
+   * @desc 创建目录
+   * @param {Function} cb - 回调
    */
   write: function (cb) {
-    // 创建目录
-    var appConf = require(this.appConfPath);
     var conf = this.conf;
-    conf.tmpName = appConf.tmpName || undefined;
-    conf.tmpId = appConf.tmpId ? appConf.tmpId : this.getTmpIdByTmpName(conf.tmpName);
     conf.moduleId = uuid.v1();
     this.mkdir(conf.moduleName);
 
     this.fs.commit(function () {
-
-      var appConfFile = fs.readFileSync(this.appConfPath);
-      var appConfStr = String(appConfFile);
-      var appConfStrLines = appConfStr.split('\n');
-      var moduleList = appConf.moduleList;
-      if (moduleList.indexOf(conf.moduleName) < 0) {
-        for (var i = 0; i < appConfStrLines.length; i++) {
-          var line = appConfStrLines[i];
-          if (line.indexOf('moduleList') >= 0) {
-            appConfStrLines[i] = line.split(']')[0];
-            if (moduleList.length > 0) {
-              appConfStrLines[i] += ', \'' + conf.moduleName + '\'],';
-            } else {
-              appConfStrLines[i] += '\'' + conf.moduleName + '\'],';
-            }
-          }
-        }
-        fs.writeFileSync(this.appConfPath, appConfStrLines.join('\n'));
-      }
       if (typeof cb === 'function') {
         cb(conf.moduleName);
       }
-      console.log();
+      console.log()
       console.log('    ' + chalk.bgGreen('模块' + conf.moduleName + '创建成功！'));
-      console.log();
+      console.log()
       console.log(chalk.yellow('    请执行 cd ' + conf.moduleName + ' 进入到模块下开始工作吧！'));
-      console.log();
-    }.bind(this));
+      console.log()
+    }.bind(this))
   },
 
   /**
@@ -142,9 +110,7 @@ var MModule = Base.extend({
    */
   create: function (cb) {
     var that = this;
-    this.getRemoteConf(function(){
-      that.talk(cb);
-    });
+    that.talk(cb);
   }
 });
 
