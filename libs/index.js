0bc82f4006f76584e97236d08ca73d08c2cd79b3
river <837606100@qq.com>
2017-11-30 14:48:44 +0800

    fix:修改目录结构

--- libs/index.js
+++ libs/index.js
@@ -35,7 +35,7 @@ var Util = {
 
   // 获取目录
   getWuuiPath: function () {
-    var wuuiPath = path.join(this.homedir(), '.wuui');
+    var wuuiPath = path.join(this.homedir(), 'wuui-cli');
     if (!this.existsSync(wuuiPath)) {
       fs.mkdirSync(wuuiPath);
     }
